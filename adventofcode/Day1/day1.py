32e704e76a5ebf466b6ab06d16a61d9865a42f33
v-arseno <v-arseno@zillowgroup.com>
2025-12-02 23:51:05 +0200

    AOC Day 1

---
+++ adventofcode/Day1/day1.py
@@ -0,0 +1,25 @@
+import os
+print(os.getcwd())
+
+pos = 50
+password = 0
+
+with open("/Users/.../input.txt") as f:
+    for x in f:
+        if x[0] == 'R':
+            for i in range(int(x[1:])):
+                pos += 1
+
+                if pos > 99:
+                    pos = 0
+        else:
+            for i in range(int(x[1:])):
+                pos -= 1
+
+                if pos < 0:
+                    pos = 99
+
+        if pos == 0:
+            password += 1
+
+print(password)
---
+++ adventofcode/Day1/pretty.py
@@ -0,0 +1,30 @@
+import os
+print(os.getcwd())
+
+def solutionDayOne():
+    pos = 50
+    password = 0
+
+    with open("/Users/.../input.txt") as file:
+        for x in file:
+            steps = int(x[1:])
+
+            pos = (pos + steps) % 100 if x[0] == 'R' else (pos - steps) % 100
+            
+            password = checkForZero(pos, password)
+                    
+    return password
+
+def checkForZero(position, password):
+    if position == 0:
+        password += 1
+    
+    return password
+
+def outOfRange(pos):
+    if pos > 99:
+        pos = 0
+    elif pos < 0:
+        pos = 99
+
+    return pos
